5573de82ee6eaaa6241c6f3d16ecc01ae596ca6b
itgeniusshuai <1115870494@qq.com>
2018-11-21 17:02:51 +0800

    完善代码

--- parse/Parser.go
+++ parse/Parser.go
@@ -8,12 +8,12 @@ import (
 
 func ParseInsertSql(sql string) ([]string,error){
 	sql = strings.TrimSpace(sql)
-	reg,_ := regexp.Compile("insert|Insert\\s+into|INTO\\s+[^\\s]+\\s*\\(([^\\)]+)\\)(values|VALUES)\\s+\\([^\\s]+\\)")
+	reg,_ := regexp.Compile(`(insert|INSERT)\s+(into|INTO)\s+([^\s]+)\(([^s]+)\)values\([^\s]+\)`)
 	match := reg.FindStringSubmatch(sql)
-	if len(match) <2{
+	if len(match) <5{
 		return nil,errors.New("非法sql")
 	}
-	insertColumns := match[1]
+	insertColumns := match[4]
 	reSep,_ := regexp.Compile("\\s*,\\s*")
 	return reSep.Split(insertColumns,-1),nil
 }
